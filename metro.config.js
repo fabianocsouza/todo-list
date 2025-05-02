d387806735ff26f9292bf978125b872067af0765
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-05-02 11:37:58 -0300

    upgrade expo 53

--- metro.config.js
+++ metro.config.js
@@ -5,14 +5,17 @@ module.exports = (() => {
 
   const { transformer, resolver } = config;
 
+  // Garantir que o transformador correto está sendo usado para arquivos SVG
   config.transformer = {
     ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
   };
+
+  // Garantir que arquivos SVG são processados corretamente no resolver
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
+    sourceExts: [...resolver.sourceExts, "svg"],
   };
 
   return config;
